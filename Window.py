85e65ecf2abf982a7c435a20bc6edbf922d77125
rickNoise <emberix@gmail.com>
2025-06-07 15:28:43 +1000

    thru CH2 L1; added Line and Point classes

--- Window.py
+++ Window.py
@@ -1,18 +1,16 @@
 from tkinter import Tk, BOTH, Canvas
+from Line import Line
 
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        """
-        It should redraw all the graphics in the window (the assumption is their positions and colors and such may have changed).
-        """
         self.__root.update_idletasks()
         self.__root.update()
 
@@ -24,3 +22,6 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from Window import Window
+from Point import Point
+from Line import Line
 
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
 
+    line1 = Line(Point(20, 20), Point(200,20))
+    line2 = Line(Point(20, 20), Point(20,200))
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "black")
+
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
